dce45ac87f3223573bb93ca48c703912ffb4aa3d
Rahul Paswan <rahulpaswanmmmut@gmail.com>
2022-12-12 07:09:38 +0000

    Update 2 files

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  function getData(val)
-  {
-    console(val)
+  const [data, setData] = useState(null);
+  const [print, setPrint] = useState(false);
+
+  function getData(val) {
+    setData(val.target.value);
+    setPrint(false);
   }
   return (
     <>
-      <h1>Hello Krishna</h1>
-      <input type= " text"  onChange= {getData} />
+      {print ? <h1> {data} </h1> : null}
+      <input type=" text" onChange={getData} />
+      <button onClick={() => setPrint(true)}>click</button>
     </>
   );
 }
